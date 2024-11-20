42b4b7a5d44e997cd3700ed7236ca9bbdae974dc
minhcao021205 <minhcao021205@gmail.com>
2024-11-20 21:21:02 +0700

    Add files via upload

---
+++ session2-04.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    
+    int canh_hinh_vuong = 5;
+
+    
+    int chu_vi = 4 * canh_hinh_vuong;
+    int dien_tich = canh_hinh_vuong * canh_hinh_vuong;
+
+    
+    printf("Chu vi hình vuông: %d\n", chu_vi);
+    printf("Dien tich hình vuông: %d\n", dien_tich);
+
+    return 0;
+}
