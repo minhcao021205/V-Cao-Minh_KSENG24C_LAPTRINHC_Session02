f4ee7080de308e1a99765d495e69dbebab1cf272
minhcao021205 <minhcao021205@gmail.com>
2024-11-20 21:10:34 +0700

    Add files via upload

---
+++ session2-02.c
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a = 5; 
+    int b = 7; 
+
+    
+    int sum = a + b;
+
+    
+    cout << "Tong cua " << a << " và " << b << " là: " << sum << endl;
+
+    return 0;
+}
