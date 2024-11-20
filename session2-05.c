0d23dd58761f04a741e21e4a4e5f7192f2be53e1
minhcao021205 <minhcao021205@gmail.com>
2024-11-20 21:24:53 +0700

    Add files via upload

---
+++ session2-05.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    
+    int chieu_dai = 10;
+    int chieu_rong = 5;
+
+    
+    int chu_vi = 2 * (chieu_dai + chieu_rong);
+    int dien_tich = chieu_dai * chieu_rong;
+
+    
+    printf("Chu vi hinh chu nhat: %d\n", chu_vi);
+    printf("Dien tich hinh chu nhat: %d\n", dien_tich);
+
+    return 0;
+}
